c60131532aad04b52b83488362e9a4e6d8dacf1b
Filippo Fontana <filippo@embrio.tech>
2025-01-28 14:06:39 +0100

    test: fix account tests

--- src/mappings/services/accountService.test.ts
+++ src/mappings/services/accountService.test.ts
@@ -1,6 +1,8 @@
 import { AccountService } from './accountService'
 
 global.getNodeEvmChainId = () => Promise.resolve('2030')
+global.isSubstrateNode = true
+global.isEvmNode = false
 
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 api.query['evmChainId'] = { chainId: jest.fn(() => ({ toString: () => '2030' })) } as any
