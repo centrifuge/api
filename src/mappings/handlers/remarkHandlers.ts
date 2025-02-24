dfbbdb27361996da58d769ffa8edf26aa1d92f2c
Filippo Fontana <filippo@embrio.tech>
2025-02-24 14:25:03 +0100

    fix: handle missingPools in remarks

--- src/mappings/handlers/remarkHandlers.ts
+++ src/mappings/handlers/remarkHandlers.ts
@@ -1,8 +1,9 @@
 import { SubstrateEvent } from '@subql/types'
-import { errorHandler } from '../../helpers/errorHandler'
+import { errorHandler, missingPool } from '../../helpers/errorHandler'
 import { RemarkEvent } from '../../helpers/types'
 import { AttestationService } from '../services/attestationService'
 import { AccountService } from '../services/accountService'
+import { PoolService } from '../services/poolService'
 
 export const handleRemark = errorHandler(_handleRemark)
 async function _handleRemark(event: SubstrateEvent<RemarkEvent>) {
@@ -11,10 +12,11 @@ async function _handleRemark(event: SubstrateEvent<RemarkEvent>) {
   const account = await AccountService.getOrInit(event.extrinsic.extrinsic.signer.toHex())
   logger.info(`Remark event fired for ${event.hash.toString()} at block ${event.block.block.header.number.toNumber()}`)
   const namedRemarks = remarks.filter((remark) => remark.isNamed)
-
   for (const namedRemark of namedRemarks) {
     const namedRemarkData = namedRemark.asNamed.toUtf8()
     const [type, poolId, attestationData] = namedRemarkData.split(':')
+    const pool = await PoolService.get(poolId)
+    if (!pool) throw missingPool
     logger.info(`Named remark with data: ${namedRemarkData}`)
     if (type === 'attestation') {
       const attestation = await AttestationService.init(
